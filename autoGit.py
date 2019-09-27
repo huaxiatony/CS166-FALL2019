ac034ac3ae1ffbd9fb13d26f83c00c47d405ec06
huaxiatony <xhua006@ucr.edu>
2019-09-27 09:06:54 -0700

    auto backup server snapshot 2019-09-27 09:06:54.998516

--- autoGit.py
+++ autoGit.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import subprocess
 import datetime
+import os
+
+goDic="cd .git"
+insertText__= "echo '[credential]'>>config"
+insertText_="echo '    helper = store'>>config&&cd .."
+os.system(goDic)
+os.system(insertText__)
+os.system(insertText_)
 
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "auto backup server snapshot " + str(datetime.datetime.now())])
