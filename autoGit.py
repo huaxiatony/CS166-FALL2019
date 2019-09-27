90afd2c19317b98f58b886e55cee2323a4ab0294
huaxiatony <xhua006@ucr.edu>
2019-09-27 09:34:31 -0700

    fixing autopush

--- autoGit.py
+++ autoGit.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import subprocess
 import datetime
+import time
 import os
 
-#goDic="cd .git"
-insertText__= "cd .git&&echo '[credential]'>>config"
-insertText_="echo '    helper = store'>>config&&cd .."
-#os.system(goDic)
-os.system(insertText__)
-os.system(insertText_)
-
-subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "auto backup server snapshot " + str(datetime.datetime.now())])
-subprocess.call(["git", "push"])
+class newRepo:
+	def __init__(self):
+		dirpath = os.getcwd()
+		insertText= "cd "+dirpath+"/.git&&echo '[credential]'>>config&&echo '    helper = store'>>config"
+		os.system(insertText)
+	def autopush(self):
+		subprocess.call(["git", "add", "."])
+		subprocess.call(["git", "commit", "-m", "auto course upload snapshot " + str(datetime.datetime.now())])
+		subprocess.call(["git", "push"])
+def main():
+	repo = newRepo
+	repo.autopush
+if __name__ == '__main__':
+    main()
